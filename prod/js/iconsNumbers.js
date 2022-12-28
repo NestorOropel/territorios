7d10ed980da0ade0184f9f3c9a89e3df6235a092
Nestor Oropel <nestororopel@gmail.com>
2022-12-28 20:57:35 -0300

    prod

---
+++ prod/js/iconsNumbers.js
@@ -0,0 +1,18 @@
+var n1 = L.icon({ iconUrl: 'number/1-c.png', iconSize: [32, 32]});
+var n2 = L.icon({ iconUrl: 'number/2-c.png', iconSize: [32, 32]});
+var n3 = L.icon({ iconUrl: 'number/3-c.png', iconSize: [32, 32]});
+var n4 = L.icon({ iconUrl: 'number/4-c.png', iconSize: [32, 32]});
+var n5 = L.icon({ iconUrl: 'number/5-c.png', iconSize: [32, 32]});
+var n6 = L.icon({ iconUrl: 'number/6-c.png', iconSize: [32, 32]});
+var n7 = L.icon({ iconUrl: 'number/7-c.png', iconSize: [32, 32]});
+var n8 = L.icon({ iconUrl: 'number/8-c.png', iconSize: [32, 32]});
+var n9 = L.icon({ iconUrl: 'number/9-c.png', iconSize: [32, 32]});
+
+const nIcons = [
+  n1, n2, n3, n4,
+  n5,
+  n6,
+  n7,
+  n8,
+  n9,
+]
