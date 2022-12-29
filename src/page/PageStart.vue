4a222b6cc171a03ed8606dcd2c55371e60246a55
Pamela <pamelayege@gmail.com>
2022-12-28 22:45:28 -0300

    Correcciones página de inicio.

--- src/page/PageStart.vue
+++ src/page/PageStart.vue
@@ -4,17 +4,17 @@
     <div class="grid w-full h-full ">
       <div class="col bg-blue-600 text-white text-left p-8 line-height-3">
         <h1 class="font-light">¡Bienvenido!</h1>
-        <h2 class="font-light">Ya puedes empezar a dibujar los mapas de tu territorio con seguridad y privacidad.</h2>
+        <h2 class="font-light">Ya puedes dibujar tus mapas de territorio de manera segura y privada.</h2>
         
         <div class="mt-5">
           <h3 class="mt-0 font-light">¿Ya tienes tu archivo .territorio?</h3>
-          <p>Solo toca abrir y buscalo en tu equipo. Recuerda tambien bajar la nueva versión una vez que termines.</p>
+          <p>Solo tienes que abrir el archivo en tu equipo y buscarlo. No olvides descargar la nueva versión una vez hayas terminado.</p>
           <FileUpload ref="fileInput" chooseLabel="Abrir" chooseIcon="pi pi-folder-open" uploadIcon="pi pi-folder-open" mode="basic" name="file" accept=".territorio" @select="fileChange" />
         </div>
 
         <div class="mt-5">
-          <h3 class="mt-0 font-light">¿Aun no tienes tu archivo .territorio?</h3>
-          <p>Solo toca Nuevo. Recuerda tambien bajar la nueva versión una vez que termines.</p>
+          <h3 class="mt-0 font-light">¿Aún no tienes tu archivo .territorio?</h3>
+          <p>Solo tienes que hacer clic en 'Nuevo'. Cuando hayas terminado, no olvides descargar la última versión.</p>
           <Button label="Nuevo"  class="p-button-success" icon="pi pi-file" @click="newDoc" />
         </div>
 
@@ -24,26 +24,26 @@
       <div class="col bg-white  p-8 text-left line-height-3">
         <h1 class="font-light">Información</h1>
         
-        <h3 class="mt-0 font-light">¿Porque es seguro y privado?</h3>
+        <h3 class="mt-0 font-light">¿Porqué es seguro y privado?</h3>
         <ul class="pl-3">
-          <li>No guarda datos.</li>
-          <li>Todos los datos se procesan en el navegador, no se procesan en servidores externos*. </li>
-          <li>Los datos solo llegan al navegador y son borrados cuando cierras la pagina.</li>
-          <li>El codigo fuente esta disponible y puedes implementarlo en tu equipo.</li>
+          <li>No almacena ningun dato.</li>
+          <li>Todos los datos se procesan en el navegador, no se procesan en servidores externos.* </li>
+          <li>Los datos solo llegan al navegador y se eliminan cuando cierras la página.</li>
+          <li>El código fuente está disponible y puedes utilizarlo en tu equipo.</li>
         </ul>
         <p class="text-xs">
           * El sistema utiliza el servicio externo OpenStreeMap para mostrar los mapas.
         </p>
 
-        <h3 class="font-light">¿Donde estan los datos?</h3>
+        <h3 class="font-light">¿Dónde estan los datos?</h3>
         <p>
-          Tomemos como ejemplo a Excel, al abrir Excel lo encontras sin datos, cargas los datos y guardas en un archivo lo que hiciste.
+          Como ejemplo, cuando abres Excel, la hoja de cálculo está vacía. Luego, cargas tus datos y los guardas en un archivo.
         </p>
         <p>
-         Excel no tiene los datos, los tiene el archivo, si queres trabajar con los datos abris el archivo en Excel. De la misma manera aqui. Tendras un archivo .territorio que tendra los datos, el archivo con los datos estara en tu equipo, no en esta aplicación web.
+          Excel no guarda los datos, sino que los encuentra en el archivo que abres. De manera similar, aquí tienes un archivo .territorio que contiene tus datos. Este archivo se encuentra en tu equipo, no en esta aplicación web.
         </p>
 
-        <h3 class="font-light">¿Porque no guardamos datos?</h3>
+        <h3 class="font-light">¿Porqué no guardamos datos?</h3>
         <ul class="pl-3">
           <li>Privacidad.</li>
           <li>Ahorro, guardar datos es caro.</li>
